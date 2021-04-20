8c072a017716e56816997360879afe7f4b5bd1e9
GitHubEason <defend.sen@gmail.com>
2021-04-20 23:50:16 +1000

    update books shop

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to BooksShop!
+          Welcome to Books Shop!
         </h1>
 
         <p className={styles.description}>
